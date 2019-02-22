7a4e94ee7d7d1bfa1c586ca2500973a13ab33cfc
= <=>
2019-02-22 15:02:40 +0500

    express basics completed

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const server = http.createServer();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+const rootDir = require('./util/path');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(rootDir,'public'))); 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(rootDir,'views','error.html'));
+})
+
+
+app.listen(3000);
